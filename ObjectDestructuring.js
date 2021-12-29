01444135bafc692866f6cc7db1f9da9e50fa581c
Arush <arushp59@gmail.com>
2021-12-29 17:31:44 +0530

    Object Destructuring

---
+++ ObjectDestructuring.js
@@ -0,0 +1,15 @@
+const myData = {
+  name: "Arush",
+  age: 26,
+  deg: "B.Tech",
+  hobbies: {
+    first: "valorant",
+    second: "anime",
+  },
+};
+
+let { name, age, deg, hobbies } = myData;
+
+console.log(
+  `My name is ${name}. My ag eis ${age}. My degree is ${deg}. My hobby is ${hobbies.second}`
+);
